020437b55941df9cfee4f9000532944dcb30ffa8
Wilgen Garcia <wilgengarcia2001@gmail.com>
2023-08-15 16:52:10 -0500

    Implementando sistemas de usuarios

---
+++ tareas-mvc/Controllers/UsuariosController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using tareas_mvc.Models;
+
+namespace tareas_mvc.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public UsuariosController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        [AllowAnonymous]
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Registro(RegistroViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var usuario = new IdentityUser() { Email = model.Email, UserName = model.Email};
+
+            var resultado = await userManager.CreateAsync(usuario, password: model.Password);
+
+            if (resultado.Succeeded)
+            {
+                await signInManager.SignInAsync(usuario, isPersistent: true);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                foreach (var error in resultado.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+
+                return View(model);
+            }
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(RegistroViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var resultado = await signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
+
+            if (resultado.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.TryAddModelError(String.Empty, "Credenciales incorrectas");
+                return View(model);
+            }
+
+        }
+
+    }
+}
