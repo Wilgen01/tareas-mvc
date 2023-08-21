23ea66c6acc15d9cce946b7bfa7795e171c378e2
Wilgen Garcia <wilgengarcia2001@gmail.com>
2023-08-21 18:06:49 -0500

    creando tareas en memoria

---
+++ tareas-mvc/wwwroot/js/tareas.js
@@ -0,0 +1,14 @@
+﻿function agregarNuevaTareaAlListado() {
+    tareasLostadoViewModel.tareas.push(new tareaElementoListadoViewModel({ id: 0, titulo: "" }))
+    $("[name=titulo-tarea]").last().focus()
+}
+
+function manejarFocusoutTituloTarea(tarea) {
+    const titulo = tarea.titulo();
+    if (!titulo) {
+        tareasLostadoViewModel.tarea.pop();
+        return
+    }
+
+    tarea.id(1);
+}
