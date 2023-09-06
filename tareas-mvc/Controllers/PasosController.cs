299d5532160fb0e34c292a1e0132ae317957e736
Wilgen Garcia <wilgengarcia2001@gmail.com>
2023-09-06 16:43:10 -0500

    creando controlador pasos

---
+++ tareas-mvc/Controllers/PasosController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using tareas_mvc.Entidades;
+using tareas_mvc.Models;
+using tareas_mvc.Servicios;
+
+namespace tareas_mvc.Controllers
+{
+    [Route("api/pasos")]
+    public class PasosController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IServicioUsuarios servicioUsuarios;
+
+        public PasosController(ApplicationDbContext context, IServicioUsuarios servicioUsuarios)
+        {
+            this.context = context;
+            this.servicioUsuarios = servicioUsuarios;
+        }
+
+        [HttpPost("{tareaId:int}")]
+        public async Task<ActionResult<Paso>> Post (int tareaId, [FromBody]PasoCrearDTO pasoCrearDTO)
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+
+            var tarea = await context.Tareas.FirstOrDefaultAsync(t => t.Id == tareaId && t.UsuarioCreacionId == usuarioId);
+
+            if (tarea == null)
+            {
+                return NotFound();
+            }
+
+            var existenPasos = await context.Pasos.AnyAsync();
+            var ordenMayor = 0;
+            if (existenPasos)
+            {
+                ordenMayor = await context.Pasos.Where(p => p.TareaId == tareaId).Select(p => p.Orden).MaxAsync();
+            }
+
+            var paso = new Paso();
+            paso.TareaId = tareaId;
+            paso.Orden = ordenMayor + 1;
+            paso.Realizado = pasoCrearDTO.Realizado;
+            paso.Descripcion = pasoCrearDTO.Descripcion;
+
+            context.Add(paso);
+
+            context.SaveChanges();
+
+
+            return paso;
+
+        }
+
+
+    }
+}
---
+++ tareas-mvc/Models/PasoCrearDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace tareas_mvc.Models
+{
+    public class PasoCrearDTO
+    {
+        [Required]
+        public string Descripcion { get; set; }
+        public bool Realizado { get; set; }
+    }
+}
