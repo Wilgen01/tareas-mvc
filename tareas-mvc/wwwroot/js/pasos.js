a3ee88c348dc2c55cd49c31c08ccc91b14c7e798
Wilgen Garcia <wilgengarcia2001@gmail.com>
2023-09-05 22:07:20 -0500

    ui agregar pasos

---
+++ tareas-mvc/wwwroot/js/pasos.js
@@ -0,0 +1,4 @@
+﻿function manejarClickAgregarPaso() {
+    editarTareaVM.pasos.push(new pasoViewModel({ modoEdicion: true, realizado: false }));
+    $("[name=txtPasoDescripcion]:visible").focus();
+}
