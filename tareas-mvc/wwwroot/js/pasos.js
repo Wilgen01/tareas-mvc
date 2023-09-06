337134bdc10c22231758bcafc1a795def8c00494
Wilgen Garcia <wilgengarcia2001@gmail.com>
2023-09-06 17:00:56 -0500

    guardando pasos

--- tareas-mvc/wwwroot/js/pasos.js
+++ tareas-mvc/wwwroot/js/pasos.js
@@ -2,3 +2,43 @@
     editarTareaVM.pasos.push(new pasoViewModel({ modoEdicion: true, realizado: false }));
     $("[name=txtPasoDescripcion]:visible").focus();
 }
+
+function manejarClickCancelarPaso(paso) {
+    if (paso.esNuevo()) {
+        editarTareaVM.pasos.pop();
+    }
+}
+
+async function manejarClickSalvarPaso(paso) {
+    paso.modoEdicion(false)
+    const esNuevo = paso.esNuevo();
+    const idTarea = editarTareaVM.id;
+
+    const data = obtenerCuerpoPeticionPaso(paso);
+
+    if (esNuevo) {
+
+    }
+}
+
+async function insertarPaso(paso, data, idTarea) {
+    const respuesta = await fetch(`${urlPasos}/${idTarea}`, {
+        body: data,
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+
+    if (respuesta.ok) {
+        const json = await respuesta.json();
+        paso.id(json.id)
+    }
+}
+
+function obtenerCuerpoPeticionPaso(paso) {
+    return JSON.stringify({
+        descripcion: paso.descripcion(),
+        realizado: paso.realizado()
+    })
+}
